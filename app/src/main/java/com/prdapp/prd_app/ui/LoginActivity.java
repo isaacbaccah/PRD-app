ba63d402427b86bf82620d1d7fd519ece73f504c
isaacbaccah <isaacbaccah@icloud.com>
2018-04-11 14:45:53 +0000

    Login In Update

--- app/src/main/java/com/prdapp/prd_app/ui/LoginActivity.java
+++ app/src/main/java/com/prdapp/prd_app/ui/LoginActivity.java
@@ -2,11 +2,13 @@ package com.prdapp.prd_app.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.prdapp.prd_app.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
