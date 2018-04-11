29779a8a046820bcc7884f6f3e65b0b64dc68c18
isaacbaccah <isaacbaccah@icloud.com>
2018-04-11 14:37:26 +0000

    First Commit

---
+++ app/src/main/java/com/prdapp/prd_app/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.prdapp.prd_app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prdapp.prd_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/prdapp/prd_app/ui/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.prdapp.prd_app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prdapp.prd_app.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
