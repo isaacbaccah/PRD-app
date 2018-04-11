66b11a28bf7329575987a1d0eeb093163ec2c801
isaacbaccah <isaacbaccah@icloud.com>
2018-04-11 14:40:09 +0000

    Main Activity

--- app/src/main/java/com/prdapp/prd_app/ui/MainActivity.java
+++ app/src/main/java/com/prdapp/prd_app/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.prdapp.prd_app;
+package com.prdapp.prd_app.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.prdapp.prd_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
