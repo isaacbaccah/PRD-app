64f006f3053484a56265cfe7788aaced54b9ec1e
isaacbaccah <isaacbaccah@icloud.com>
2018-04-11 14:38:39 +0000

    Splash Screen

---
+++ app/src/main/java/com/prdapp/prd_app/ui/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.prdapp.prd_app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prdapp.prd_app.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
